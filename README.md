c710f1c0c5578da4a12fb72a18be87e3f767498a
Ron Zandbergen <ron@insight-tools.com>
2018-12-21 16:00:46 -0600

    Add clean-address.py
    First pass at cleaning addresses

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Insight Tools Python Tools
 Tools to allow me to better manage the various technical tasks associated with
 the Insight Safari research
 
-clean-address.py
+clean-addresses.py
   - take a list of addresses, pass it to Googles geocoder and write out a 
     nice clean spreadsheet
---
+++ clean-addresses.py
@@ -0,0 +1,93 @@
+#
+# clean-address.py
+#
+# Python script which takes a csv file with a list of raw mailing addresses
+# uses the Google geocoder API to clean then break into the following fields:
+#
+#   - House number
+#   - Street
+#   - Apt / Unit / etc
+#   - City
+#   - State
+#   - Zip Code
+#
+# and then write the results to a csv file
+#
+
+# set up the Google Geocoding API key.  This needs to be done before importing 
+# the geocoder library
+
+import os
+os.environ["GOOGLE_API_KEY"] = "AIzaSyBwqdWMfytQAuwLzG5MXmgZ9oxLbKYzTxY"
+
+# Import the needed libraries
+
+import geocoder as geo
+import pandas as pd
+
+# Open the input file
+
+inputFile = pd.read_csv('e:\Downloads\Raw Addresses.csv')
+
+col_headings = ['First Name',
+                'Last Name',
+                'Street Address',
+                'Unit',
+                'City',
+                'State',
+                'Zip',
+                'Email',
+                'Phone']
+
+outputFile = pd.DataFrame(columns=col_headings)
+
+# Iterate through each entry and have Google clean up the address
+
+for index, row in inputFile.iterrows() :
+    address_string = row['Street Address'] + ' ' + str(row['City']) + ' ' + str(row['State']) + ' ' + str(row['Zip'])
+    g = geo.google(address_string)
+
+# Handle missing data
+    
+    try:
+        houseNumber = g.osm['addr:housenumber']
+    except:
+        houseNumber = '<missing houseNumber>'
+        
+    try:
+        street = g.osm['addr:street']
+    except:
+        street = '<missing street>'
+        
+    try:
+        city = g.osm['addr:city']
+    except:
+        city = '<missing city>'
+        
+    try:
+        state = g.osm['addr:state']
+    except:
+        state = '<state>'
+        
+    try:
+        zip = g.osm['addr:postal']
+    except:
+        zip = '<missing postal'
+        
+# Build the new row    
+    
+    new_row = {'First Name' : row['First Name'].title(),
+               'Last Name' : row['Last Name'].title(),
+               'Street Address' : houseNumber + ' ' + street,
+               'Unit' : row['Unit'],
+               'City' : city,
+               'State' : state,
+               'Zip' : zip,
+               'Email' : row['email'].lower(),
+               'Phone' : row['Phone']}
+    
+    outputFile = outputFile.append(new_row, ignore_index=True)
+
+writer = pd.ExcelWriter('e:\Downloads\Cleansed Addresses.xlsx')
+outputFile.to_excel(writer, 'Cleansed Addresses')
+writer.save()
